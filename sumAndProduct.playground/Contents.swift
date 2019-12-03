3f6e97e223044e4daa430a956a6103b492c495b6
David Williams <burmesedragon@att.net>
2019-12-02 20:52:56 -0800

    @jds655 Mostly works! :)

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,26 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+    var sumAndProduct: [Int] = []
+    let a: UInt = product % sum
+    var b: UInt = sum % product
+    let x: UInt = sum
+    let y: UInt = product
+    if a != 0 {
+        sumAndProduct.append(Int(a))
+        if b % a == 0 {
+            b = a % b
+        } else {
+            b = a % b
+        }
+        print(a, b)
+        print(a+b, a*b)
+        print(sumAndProduct)
+        sumAndProduct.append(Int(b))
+        return [y % x]
+    } else {
+    return []
+    }
 }
+sumAndProduct(14, 49)
+
